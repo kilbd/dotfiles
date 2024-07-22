18adf9c31f120fbefdaf492abf8ee0f1dea10cf2
Drew Kilb <git@kilb.email>
2024-07-21 21:25:51 -0400

    fix: Handle errors with checking out working files

--- setup.sh
+++ setup.sh
@@ -7,25 +7,28 @@ sudo apt install -y libssl-dev openssl pkg-config build-essential git curl wget 
 
 # Install Rust(up)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+. "$HOME/.cargo/env"
 
 # Install Rust tools
 cargo install bat difftastic eza ripgrep zellij
 cargo install --locked --bin jj jj-cli
 
 # Clone dotfiles as a bare repository
-git clone --bare https://github.com/kilbd/dotfiles.git $HOME/.dotfiles
+git clone --bare https://github.com/kilbd/dotfiles.git "$HOME/.dotfiles"
 function dotfiles {
-   /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME $@
+   /usr/bin/git --git-dir="$HOME/.dotfiles" --work-tree="$HOME $@"
 }
-dotfiles checkout
-# Move pre-existing dotfiles if there was an error
-if [ $? = 0 ]; then
+
+set +e
+# Move pre-existing dotfiles if error checking out
+if dotfiles checkout; then
   echo "Checked out dotfiles.";
 else
   echo "Backing up pre-existing dotfiles.";
   mkdir -p .dotfiles-backup
-  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+  dotfiles checkout 2>&1 | grep -E "\s+\." | awk "{'print $1'}" | xargs -I{} mv {} .dotfiles-backup/{}
 fi
+set -e
 
 dotfiles checkout
 dotfiles config --local status.showUntrackedFiles no
